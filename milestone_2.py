f31663bd3c735287328b144de51bcbf914500e41
Evelina <evelina.miezanskaite@gmail.com>
2023-10-29 21:37:33 +0000

    Asks user to input single letters for a randomly picked word from a list

---
+++ milestone_2.py
@@ -0,0 +1,16 @@
+# %%
+import random
+word_list = ["banana", "kiwi", "grapes", "pineapple", "clementines"]
+# print(word_list)
+word = random.choice(word_list)
+print(word)
+
+# %%
+guess = input ("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess!")
+else:
+    print("Oops, that is not a valid input")
+
+# %%
